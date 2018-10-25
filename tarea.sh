7147a4cd915402f6674c2ff7e1389f2a18378c4f
yuniquintero <yuni.quintero@gmail.com>
2018-10-24 23:45:56 -0400

    pasos 2 y 3

--- tarea.sh
+++ tarea.sh
@@ -12,12 +12,29 @@ fi
 
 while true
     do 
-    ping -c 1 ldc.usb.ve 2> /dev/null
+    ping -c 1 $1 2> /dev/null
     if [ $? == 0 ]
     then
-        echo "hola"
+        echo "url is up"
     else
-        echo "chao"
+        gtway=$(ip route | egrep default | cut -d' ' -f 3)
+        echo $gtway
+        ping -c 1 $gtway 2> /dev/null
+        if [ $? == 0 ]
+        then
+            echo "gateway is up, url down"
+        else
+            priv_ip=$(ip addr show wlan0 | grep -Po 'inet \K[\d.]+')
+            echo $priv_ip
+            ping -c 1 $priv_ip 2> /dev/null
+            
+            if [ $? == 0 ]
+            then
+                echo "ip private is up, gateway is down"
+            else
+                echo "chao"
+            fi
+        fi
     fi
     sleep 5
 done
