37f6a4edb44088b48c1c64b7f61daa4b0fac676c
German Robayo Paz <germanrobayo33@gmail.com>
2018-10-25 22:57:28 -0400

    termina (?) la tarea

--- tarea.sh
+++ tarea.sh
@@ -36,41 +36,40 @@ fi
 url=$1
 while true
     do
-    message
+    message=""
     # Hacemos ping al dominio para ver si esta up.
     pingdomain $url
     # El codigo de retorno 2 indica que el dominio
-    # suministrado no fue resolvido por un dns. El problema puede ser
+    # suministrado no fue resuelto por un dns. El problema puede ser
     # el dns del dominio
-    if [ $? == 2 ]
-    then
+    if [ $? = 2 ]; then
         message="La direccion ip de $url no fue resolvida"
     # Si es 1, es que el dominio no le dio respuesta.
-    else if [ $? == 1 ]
+    elif [ $? = 1 ]; then
         message="El dominio $url esta down"
         gtway=$(getGateway)
         # Si el gtway es "" es por que la computadora esta en una red
-        if [ "$gtway" = "" ]
-        then
+        if [ "$gtway" = "" ]; then
             message="La red de la computadora local no tiene un gateway asignado"
         # En otro caso, hacemos ping a ver si el gateway esta vivo
         else
             pingdomain $gtway
-            if [ $? != 0 ]
-            then
+            if [ $? != 0 ]; then
                 message="Hay un problema con el gateway de la red"
                 # Pudieramos ver si el problema es que no tenemos ip asignada
                 # Para ver eso, vemos la interfaz que se esta usando
                 interface=$(getDefaultInterface)
                 priv_ip=$(getMachineIp)
-                if []
-                pingdomain $priv_ip
-
-                if [ $? == 0 ]
-                then
-                    echo "ip private is up, gateway is down"
+                if [ "$interface" = "" ]; then
+                    message="La interfaz de red no esta funcionando correctamente"
                 else
-                    echo "chao"
+                    if [ "$priv_ip" = "" ]; then
+                        message="La computadora local no tiene ip asignada"
+                    else
+                        true;
+                        # Si pasa por aca, es culpa del gateway. Ya el mensaje
+                        # se asigno arriba.
+                    fi
                 fi
             fi
         fi
